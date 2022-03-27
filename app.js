83b6c286a3f7e543f519ddbf8421cf5503f9a1c8
valerieroman <81587687+valerieroman@users.noreply.github.com>
2022-03-26 22:00:01 -0400

    Started adding js

---
+++ app.js
@@ -0,0 +1,18 @@
+// classList - shows/gets all classes
+
+// contains - checks classList for specific class
+
+// add - remove class
+
+// remove - remove class
+
+// toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+    //console.log(links.classList);
+    console.log(links.classList.contains('random'));
+});
